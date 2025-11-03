fc0e176decda1b2190ecd5dc4b6b7c990c26d0ae
bencasolocaso <be.serranoh@duocuc.cl>
2025-11-03 14:50:43 -0300

    m

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PruebaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Androi",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
